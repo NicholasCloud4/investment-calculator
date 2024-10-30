51a6ca50ab8c1246c890740e57b19a021656c2b1
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-10-30 10:34:30 -0400

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div id='header'>
+            <h1>React Investment Calculator</h1>
+            <img src="./investment-calculator-logo.png" alt="" />
+        </div>
+    )
+}
+
+export default Header
